8cb1d3603c5287e3222b9ec207dc34c4fdfa7fae
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-08-14 14:47:20 +0500

    Add files via upload

---
+++ q5.ts
@@ -0,0 +1,4 @@
+let famous_person: string = "Quaid-e-Azam";
+let message:string = "There are two powers in the world; one of the sword and other is the pen";
+
+console.log(`${famous_person}, once said  ${message}`);
